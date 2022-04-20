5bf40175baebc7a28ed8f37f83e50c90fa874d56
Jonathan Pope <jonathanmpope@gmail.com>
2022-04-20 14:19:10 -0600

    Update arrays

--- friends.rb
+++ friends.rb
@@ -3,3 +3,7 @@ arrays = ["Bob", "Jill", "Barak", "Cindy", "Jonathan"]
 p arrays[2]
 p arrays[-1]
 p arrays[0]
+
+cities = ["Denver", "Chicago", "Austin", "New York", "San Francisco"]
+
+p cities.pop
